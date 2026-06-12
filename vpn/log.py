b2b080d93bee6cd0d2fd4ea7cfdb3257f200d55a
Andrew Li <andrewli@u.northwestern.edu>
2026-06-12 01:43:42 -0400

    improve colors and format of log messages

--- vpn/log.py
+++ vpn/log.py
@@ -1,4 +1,12 @@
+import copy
 import logging
+from enum import StrEnum
+
+
+class SentinelColor(StrEnum):
+    GRAY = "\033[37m"
+    BOLD_WHITE = "\033[1;97m"
+    RESET = "\033[0m"
 
 
 class ColoredFormatter(logging.Formatter):
@@ -8,32 +16,34 @@ class ColoredFormatter(logging.Formatter):
             case logging.DEBUG:
                 return "\033[2m"
             case logging.INFO:
-                return "\033[36m"
+                return "\033[92m"
             case logging.WARNING:
-                return "\033[33m"
+                return "\033[93m"
             case logging.ERROR:
-                return "\033[31m"
+                return "\033[91m"
             case logging.CRITICAL:
-                return "\033[1;31m"
+                return "\033[48;2;220;20;60m\033[97m"
             case _:
                 return ""
 
-    @staticmethod
-    def reset_color() -> str:
-        return "\033[0m"
-
     def format(self, record: logging.LogRecord) -> str:
-        color = self.color_of_loglevel(record.levelno)
-        return f"{color}{super().format(record)}{self.reset_color()}"
+        reset = SentinelColor.RESET
+
+        record = copy.copy(record)
+
+        timestamp = self.formatTime(record, self.datefmt)
+        level = f"{self.color_of_loglevel(record.levelno)}{record.levelname:<8}{reset}"
+        name = f"{SentinelColor.BOLD_WHITE}{record.name}{reset}"
+        message = record.getMessage()
+
+        return f"{SentinelColor.GRAY}{timestamp}{reset}  {level}  {name}{SentinelColor.GRAY}: {message}{reset}"
 
 
 def get_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
     if not logger.handlers:
         handler = logging.StreamHandler()
-        handler.setFormatter(
-            ColoredFormatter("%(asctime)s [%(levelname)-8s] %(name)s: %(message)s")
-        )
+        handler.setFormatter(ColoredFormatter())
         logger.addHandler(handler)
         logger.setLevel(logging.DEBUG)
     return logger
